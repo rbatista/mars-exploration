cf666d41b4825473daff681d4cf0764ff0017639
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2019-02-10 13:53:03 -0200

    Add spring-boot

---
+++ src/main/java/com/raphaelnegrisoli/elo7/marsexploration/MarsExplorationApplication.java
@@ -0,0 +1,14 @@
+package com.raphaelnegrisoli.elo7.marsexploration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsExplorationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsExplorationApplication.class, args);
+	}
+
+}
+
