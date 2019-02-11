f182a21aed7cfe3a5f383fe014c7eadfa1463601
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2019-02-10 22:47:41 -0200

    Add Exception handler

---
+++ src/main/java/com/raphaelnegrisoli/elo7/marsexploration/controller/ExceptionHandlerController.java
@@ -0,0 +1,44 @@
+package com.raphaelnegrisoli.elo7.marsexploration.controller;
+
+import com.raphaelnegrisoli.elo7.marsexploration.controller.dto.ErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlerController.class);
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ErrorResponse handleInternalServerError(final Exception e) {
+
+        LOGGER.error("Internal Server Error", e);
+        return new ErrorResponse("Internal Server Error");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseBody
+    public ErrorResponse handleIllegalArgumentException(final IllegalArgumentException e) {
+
+        LOGGER.error("Bad request", e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseBody
+    public ErrorResponse handleIllegalStateException(final IllegalStateException e) {
+
+        LOGGER.error("Bad request", e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+}
--- src/main/java/com/raphaelnegrisoli/elo7/marsexploration/service/ProbeCommandService.java
+++ src/main/java/com/raphaelnegrisoli/elo7/marsexploration/service/ProbeCommandService.java
@@ -20,4 +20,5 @@ public class ProbeCommandService {
         command.execute(probe);
         probeService.save(probe);
     }
+
 }
